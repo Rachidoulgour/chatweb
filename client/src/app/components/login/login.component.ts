1a0e43de700bed803f0e20f22acc6679a093e05c
rachid <olgor@hotmail.com>
2020-12-07 18:59:59 +0100

    user service and interface added

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService} from '../../services/user.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  error404;
+  user={
+    email: "",
+    password: ""
+  }
 
-  constructor() { }
+  constructor(private userService: UserService, private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+  }
+  login(){
+    
+    this.userService.login(this.user).subscribe(
+      res=>{
+        console.log(res)
+        localStorage.setItem('token', res.token);
+        localStorage.setItem('user', JSON.stringify(res.user));
+        this.router.navigate(['/perfil'])
+      },
+      err=>{
+        console.log(err)
+        if(err.status === 404){
+          this.error404=err.status
+        }
+      }
+    )
   }
 
 }
---
+++ client/src/app/interfaces/User.ts
@@ -0,0 +1,11 @@
+export interface User{
+    _id: string,
+    username: string,
+    email: string,
+    password: string,
+    city: string,
+    role: string,
+    avatar:string,
+    confirmedEmail: boolean
+
+}
